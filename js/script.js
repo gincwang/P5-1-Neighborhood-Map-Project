9751a770d11f984a42407b345e69a1a874181258
gincwang <gincwang@gmail.com>
2015-09-10 19:02:54 -0700

    Add side list for showing location info

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var LocationModel = function() {
     var self = this;
 
     self.markers = [];
-    self.markerInfos = ko.observableArray();
+    self.markersInfo = ko.observableArray();
 }
 
 
@@ -14,6 +14,7 @@ var NeighborhoodViewModel = function() {
 
     self.locations = new LocationModel();
     self.map = null;
+    self.selectedMarker = null;
 
     self.init = function() {
         self.createMap();
@@ -44,34 +45,47 @@ var NeighborhoodViewModel = function() {
        var searchBox = new google.maps.places.SearchBox(input);
        self.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
+       //populate
+       var sideList = document.getElementById("side-list");
+       self.map.controls[google.maps.ControlPosition.LEFT_TOP].push(sideList);
+
        // Bias the SearchBox results towards current map's viewport.
        self.map.addListener('bounds_changed', function() {
+         console.log("bounds_changed()");
          searchBox.setBounds(self.map.getBounds());
        });
 
-       self.locations.markers = [];
 
        // Listen for the event fired when the user selects a prediction and retrieve
        // more details for that place.
        searchBox.addListener('places_changed', function() {
+          console.log("places_changed()");
           var places = searchBox.getPlaces();
 
           if (places.length == 0) {
+            //no match was found
            return;
           }
 
-          // Clear out the old markers.
+          // Clear out the old markers and list location info
           self.locations.markers.forEach(function(marker) {
               marker.setMap(null);
           });
           self.locations.markers = [];
+          self.locations.markersInfo([]);
 
           // For each place, get the icon, name and location.
           var bounds = new google.maps.LatLngBounds();
-          var count = 0;
+          var count = 0;    //counter for marker animation delay when dropped
           places.forEach(function(place) {
-
+              console.log(place);
               count++;
+
+              self.locations.markersInfo.push({
+                  name: place.name,
+                  address: place.formatted_address}
+              );
+
               var icon = {
                   url: place.icon,
                   size: new google.maps.Size(50, 50),
@@ -80,9 +94,8 @@ var NeighborhoodViewModel = function() {
                   scaledSize: new google.maps.Size(25, 25)
               };
 
-
+              //add marker to marker array for each place onto the map
               setTimeout(function(){
-                  // Create a marker for each place.
                   self.locations.markers.push(new google.maps.Marker({
                       map: self.map,
                       icon: icon,
@@ -108,6 +121,7 @@ var NeighborhoodViewModel = function() {
 
     }
 
+    //get the map started with the init()
     self.init();
 
 }
