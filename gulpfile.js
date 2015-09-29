61601e4a15c259cf9fdac7123550c1bb286db0b9
gincwang <gincwang@gmail.com>
2015-09-29 15:44:55 -0700

    add gulp files

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    minCSS = require('gulp-minify-css');
+
+gulp.task('minifyJS', function() {
+    return gulp.src('js/*.js')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts'))
+});
+
+gulp.task('minifyCSS', function(){
+    return gulp.src('css/*.css')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minCSS())
+        .pipe(gulp.dest('dist/css'))
+});
